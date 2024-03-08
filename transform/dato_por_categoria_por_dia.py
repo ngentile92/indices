7d296261661aae97f56ae7836754a5cc2ca343be
Nicolas Gentile <nag.gentile@gmail.com>
2024-03-08 20:31:39 -0300

    logica inicial de indice

--- transform/dato_por_categoria_por_dia.py
+++ transform/dato_por_categoria_por_dia.py
@@ -1,6 +1,7 @@
 from extract.db import fetch_data
 from transform.ponderadores import ponderadores_inflacion_actualizado, ponderadores_alquileres
 import ast
+import pandas as pd
 
 query_precios_supermercado = """
 SELECT p.date, cp.categoria_indice, sum(p.precio)/count(p.producto) as precio_promedio
@@ -46,3 +47,91 @@ dolar['promedio'] = dolar['dolar_blue'].apply(corregir_valores)
 
 
 
+# Re-definir la función de conversión
+def convert_currency(row):
+    if row['moneda_alquiler'] == 'USD':
+        row['alquiler'] = row['alquiler'] * row['promedio']
+        row['moneda_alquiler'] = '$'  # Cambiar la moneda a pesos argentinos después de la conversión
+    if row['moneda_expensas'] == 'USD' and pd.notnull(row['expensas']):
+        row['expensas'] = row['expensas'] * row['promedio']
+        row['moneda_expensas'] = '$'
+        pass
+    return row
+
+# Corregir el tipo de dato de 'date' en df2 para que coincida con df1
+dolar['date'] = dolar['date'].dt.date
+alquileres['date'] = alquileres['date'].dt.date
+# Intentar el merge nuevamente
+merged_df = alquileres.merge(dolar, on='date', how='left')
+
+df_alquileres = merged_df.apply(convert_currency, axis=1)
+
+
+def aplicar_ponderador(row):
+    # Obtener el ponderador para la localidad de la fila
+    ponderador = ponderadores_alquileres.get(row['localidad'], 1)  # Usar 1 como valor predeterminado si no se encuentra la localidad
+    # Aplicar el ponderador al valor del alquiler
+    row['alquiler'] *= ponderador
+    return row
+
+# Aplicar la función a cada fila del DataFrame
+df_alquileres_ponderados = df_alquileres.apply(aplicar_ponderador, axis=1)
+
+df_alquileres_ponderados[['id', 'date', 'alquiler', 'expensas', 'localidad']]
+
+#group by date and sum alquiler
+df_alquileres_ponderados = df_alquileres_ponderados.groupby('date').agg({'alquiler': 'mean', 'expensas': 'mean'}).reset_index()
+
+
+# Paso 1: Crear DataFrame completo con rango de fechas
+rango_fechas = pd.date_range(start=df_alquileres_ponderados['date'].min(), end=df_alquileres_ponderados['date'].max())
+df_fechas_completo = pd.DataFrame({'date': rango_fechas})
+
+# Convertir la columna 'date' en df_fechas_completo y df_alquileres_ponderados a datetime64[ns] si aún no lo son
+df_fechas_completo['date'] = pd.to_datetime(df_fechas_completo['date'])
+df_alquileres_ponderados['date'] = pd.to_datetime(df_alquileres_ponderados['date'])
+
+# Ahora que ambos tienen el mismo tipo de dato en 'date', intentar hacer el merge nuevamente
+df_merge = df_fechas_completo.merge(df_alquileres_ponderados, on='date', how='left')
+
+# Paso 3: Llenar valores faltantes manualmente para cada columna numérica
+for col in ['alquiler', 'expensas']:
+    # Identificar índices de filas con valores NaN
+    indices_nan = df_merge[df_merge[col].isna()].index
+    
+    for i in indices_nan:
+        # Encontrar el valor anterior y posterior no-NaN más cercanos
+        prev_val = df_merge[col][:i].dropna().last_valid_index()
+        next_val = df_merge[col][i:].dropna().first_valid_index()
+        
+        # Calcular el promedio si ambos valores existen, sino usar el valor disponible
+        if pd.notna(prev_val) and pd.notna(next_val):
+            df_merge.loc[i, col] = (df_merge.loc[prev_val, col] + df_merge.loc[next_val, col]) / 2
+        elif pd.notna(prev_val):
+            df_merge.loc[i, col] = df_merge.loc[prev_val, col]
+        elif pd.notna(next_val):
+            df_merge.loc[i, col] = df_merge.loc[next_val, col]
+
+# El DataFrame df_merge ahora tiene las fechas completadas y los valores faltantes rellenados
+df_merge['date'] = pd.to_datetime(df_merge['date'])
+
+# Encontrar la fecha máxima en df_merge y la fecha de hoy
+ultima_fecha = df_merge['date'].max()
+fecha_hoy = pd.Timestamp('today').normalize()  # Normalizar para obtener solo la fecha sin componente de tiempo
+
+# Si la última fecha en df_merge es antes de hoy, necesitamos agregar filas hasta hoy
+if ultima_fecha < fecha_hoy:
+    # Crear un DataFrame con el rango de fechas faltantes hasta hoy
+    fechas_adicionales = pd.date_range(start=ultima_fecha + pd.Timedelta(days=1), end=fecha_hoy)
+    df_adicionales = pd.DataFrame({'date': fechas_adicionales})
+    
+    # Replicar los últimos valores conocidos de 'alquiler' y 'expensas' para las nuevas fechas
+    ultimos_valores = df_merge.iloc[-1][['alquiler', 'expensas']].to_dict()
+    df_adicionales = df_adicionales.assign(**ultimos_valores)
+    
+    # Concatenar el DataFrame original con el de fechas adicionales
+    df_merge_completo = pd.concat([df_merge, df_adicionales], ignore_index=True)
+else:
+    df_merge_completo = df_merge
+
+df_merge_completo  # Mostrar las últimas filas para verificar el resultado
